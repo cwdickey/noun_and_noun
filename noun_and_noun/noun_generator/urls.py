f66ffda4edb1b2bf5ae4a002265125df46149ada
Charlie Dickey <charlie@Charlies-MacBook-Pro.local>
2018-07-01 17:21:47 -0700

    added style.css and updated index.html to render context

--- noun_and_noun/noun_generator/urls.py
+++ noun_and_noun/noun_generator/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from django.contrib import admin
 from . import views
 
 urlpatterns = [
--- noun_and_noun/noun_generator/views.py
+++ noun_and_noun/noun_generator/views.py
@@ -1,12 +1,14 @@
 #from django.shortcuts import render
 from django.http import HttpResponse
 import random
+from django.shortcuts import render
 
 noun_list = ['Flower', 'Water', 'Basil', 'Hazelnut', 'Thistle', 'Thread', 'Herb', 'Wood']
 and_symbol_list = [' & ', ' + ']
 
 
 def index(request):
+    template = 'noun_generator/index.html'
     noun_one = random.choice(noun_list)
     same_noun = True
     while same_noun is True:  # could also copy the noun list, pop a value, and get a second value from copied list
@@ -14,4 +16,9 @@ def index(request):
         if noun_one != noun_two:
             same_noun = False
     and_symbol = random.choice(and_symbol_list)
-    return HttpResponse(noun_one + and_symbol + noun_two)
+    context = {'noun_text': noun_one + ' ' + and_symbol + ' ' + noun_two}
+
+    # download background image
+    #download_url = 'https://farm%s.staticflickr.com/%s/%s_%s.jpg' % farm_id, server_id, idnum, secret_id
+
+    return render(request, template, context)
